1cf8e6b3701c4322ad8fb85ca1918c19f76fb688
xiaweizi <1012126908@qq.com>
2019-01-03 17:39:40 +0800

    [dev] 初始化项目

---
+++ app/src/main/java/con/xiaweizi/dashboardview/DashBoardView.java
@@ -0,0 +1,22 @@
+package con.xiaweizi.dashboardview;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * <pre>
+ *     author : xiaweizi
+ *     class  : con.xiaweizi.dashboardview.DashBoardView
+ *     e-mail : 1012126908@qq.com
+ *     time   : 2019/01/03
+ *     desc   :
+ * </pre>
+ */
+public class DashBoardView extends View {
+
+    public DashBoardView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
